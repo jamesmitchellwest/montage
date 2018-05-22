26cba065eb74089599915bfd43653479ab182cec
Jim <imojim@gmail.com>
2018-05-22 01:10:16 -0500

    Remove the now ignored directory .cache

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "C:/src/montage/src/layouts/index.js"
-  import data from "C:\\src\\montage\\.cache\\json\\layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
